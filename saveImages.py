8333e69356834740a73d4273fe6b8726570ef751
CleanestMink126 <gns126@gmail.com>
2018-11-11 20:29:07 -0500

    tesitng

--- saveImages.py
+++ saveImages.py
@@ -3,21 +3,21 @@ import numpy
 
 
 def show_cam():
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
 
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
 
         # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # Display the resulting frame
-        # cv2.imshow('frame',gray)
-        cv2.imwrite('./test.jpg', gray)
+        cv2.imshow('frame',frame)
+        # cv2.imwrite('./../test.jpg', gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        exit()
+        # exit()
 
     # When everything done, release the capture
     cap.release()
